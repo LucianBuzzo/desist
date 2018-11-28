388c95d1c286bb32a8b9c4c49c1ae5e33eb3561c
Lucian <lucian.buzzo@gmail.com>
2018-11-28 22:13:27 +0000

    Add standardJS and lint main file
    Change-type: patch
    Signed-off-by: Lucian <lucian.buzzo@gmail.com>

--- desist.js
+++ desist.js
@@ -1,40 +1,40 @@
-!(function() {
-  var sizes = [];
-  var created = false;
-  var overlay = document.createElement('div');
-  var createOverlay = function() {
-    created = true;
-    overlay.style.position = 'fixed';
-    overlay.style.top = 0;
-    overlay.style.left = 0;
-    overlay.style.right = 0;
-    overlay.style.bottom = 0;
-    overlay.style['z-index'] = 999999999999;
-    overlay.style['background-color'] = 'red';
-    overlay.style.color = 'white';
-    overlay.style['font-size'] = '148px';
-    overlay.style['letter-spacing'] = '4px';
-    overlay.style['padding-top'] = '200px';
-    overlay.style['text-align'] = 'center';
-    overlay.innerText = 'STOP IT!';
-    document.body.appendChild(overlay);
-  };
-  var hideTimeout;
-  setInterval(function() {
-    sizes.push(window.innerWidth);
+(function () {
+  var sizes = []
+  var created = false
+  var overlay = document.createElement('div')
+  var createOverlay = function () {
+    created = true
+    overlay.style.position = 'fixed'
+    overlay.style.top = 0
+    overlay.style.left = 0
+    overlay.style.right = 0
+    overlay.style.bottom = 0
+    overlay.style['z-index'] = 999999999999
+    overlay.style['background-color'] = 'red'
+    overlay.style.color = 'white'
+    overlay.style['font-size'] = '148px'
+    overlay.style['letter-spacing'] = '4px'
+    overlay.style['padding-top'] = '200px'
+    overlay.style['text-align'] = 'center'
+    overlay.innerText = 'STOP IT!'
+    document.body.appendChild(overlay)
+  }
+  var hideTimeout
+  setInterval(function () {
+    sizes.push(window.innerWidth)
     if (sizes.filter((v, i, a) => a.indexOf(v) === i).length > 10) {
-      console.error('STOP IT');
-      window.clearTimeout(hideTimeout);
+      console.error('STOP IT')
+      window.clearTimeout(hideTimeout)
       if (created) {
-        overlay.style.display = 'block';
+        overlay.style.display = 'block'
       } else {
-        createOverlay();
+        createOverlay()
       }
 
-      hideTimeout = setTimeout(function() {
-        overlay.style.display = 'none';
-      }, 500);
+      hideTimeout = setTimeout(function () {
+        overlay.style.display = 'none'
+      }, 500)
     }
-    sizes = sizes.slice(-30);
-  }, 100);
-}());
+    sizes = sizes.slice(-30)
+  }, 100)
+}())
